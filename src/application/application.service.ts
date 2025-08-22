6a435c7a76dd2363a2f44e5bf0e109912d607fc8
rommelmemin7 <rommel_memin@hotmail.com>
2025-08-21 20:12:27 -0500

    Changes DEUNA v2

--- src/application/application.service.ts
+++ src/application/application.service.ts
@@ -226,6 +226,7 @@ export class ApplicationService {
 				approvedAt: true,
 				externalStatus: true,
 				observation: true,
+				referenceTransaction: true,
 				payment: {
 					select: {
 						id: true,
